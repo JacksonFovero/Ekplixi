089631266a98f97c15f0ddfaf3a33ba92a681f4e
Jackson <jaesthetyc@gmail.com>
2018-05-26 21:25:50 -0500

    Update 39

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const settings = require("./settings.json");
 const pack = require("./package.json");
 const prefix = settings.prefix;
 var testing = settings.testing;
-const colors = require("colors");
+const chalk = require("chalk");
 const cmds = new Discord.RichEmbed()
 	.setTitle("Command List")
 	.setColor(settings.color)
@@ -18,35 +18,35 @@ const cmds = new Discord.RichEmbed()
 // END VARIABLES
 // BEGIN STARTUP
 client.on("ready", () => {
-	console.log("    ________         ___      _ ".rainbow.bold);
-	console.log("   / ____/ /______  / (_)  __(_)".rainbow.bold);
-	console.log("  / __/ / //_/ __ |/ / / |/_/ / ".rainbow.bold);
-	console.log(" / /___/ ,< / /_/ / / />  </ /  ".rainbow.bold);
-	console.log("/_____/_/|_/ .___/_/_/_/|_/_/   ".rainbow.bold);
-	console.log("          /_/                   ".rainbow.bold);
-	console.log("████████████████████████████████".rainbow.bold);
-	console.log("By JacksonFovero.".cyan.bold.trap);
+	console.log(chalk.hex("#ffffff")("    ________         ___      _ "));
+	console.log(chalk.hex("#ffffff")("   / ____/ /______  / (_)  __(_)"));
+	console.log(chalk.hex("#ffffff")("  / __/ / //_/ __ |/ / / |/_/ / "));
+	console.log(chalk.hex("#ffffff")(" / /___/ ,< / /_/ / / />  </ /  "));
+	console.log(chalk.hex("#ffffff")("/_____/_/|_/ .___/_/_/_/|_/_/   "));
+	console.log(chalk.hex("#ffffff")("          /_/                   "));
+	console.log("");
+	console.log(chalk.cyan("By JacksonFovero."));
 	console.warn("Ekplixi is in its alpha phase. The console has not been set up as of " + pack.version + ".");
 	console.log(" ");
 	console.log("--Command Handler Enabled--");
 	console.log(" ");
 	if (settings.token === "NDQ5NzMwNzMyOTE5NjE5NTg0.Deo75w.LWG7FjVVhbe3aYjF1-xd4RpxXL8") {
-		console.log("OOF: ".bold.red + "You have not set up settings.json. You are currently using the test bot account, which is in no servers and is not public. The bot will shut down in 15 seconds.");
+		console.log(chalk.red("OOF: ") + "You have not set up settings.json. You are currently using the test bot account, which is in no servers and is not public. The bot will shut down in 15 seconds.");
 		setTimeout(function() {client.destroy();}, 15000);
 	}
 	if (testing == "true") {
 		client.user.setStatus("idle");
 		client.user.setActivity("on a test build.");
-		console.warn("! You have indicated you are on a test build, you are the only person that can run commands!".bold.yellow);
-		console.warn("! You can change this in settings.json.".bold.yellow.underline);
+		console.warn(chalk.yellow("! You have indicated you are on a test build, you are the only person that can run commands!"));
+		console.warn(chalk.yellow("! You can change this in settings.json."));
 	}
 	else if (testing == "false") {
 		client.user.setStatus("Online");
 		client.user.setActivity("on a stable build!");
-		console.log("You have indicated you are on a " + "stable".green.bold + " build.");
+		console.log("You have indicated you are on a " + chalk.green("stable") + " build.");
 	}
 	else {
-		console.log("settings.testing is neither true or false. The bot has shut down.".red.bold.underline);
+		console.log(chalk.red.underline("settings.testing is neither true or false. The bot has shut down."));
 		client.destroy();
 		setTimeout(function() {
 			process.exit();
@@ -93,12 +93,12 @@ client.on("message", message => {
 	if (command === "ping") {
 		message.channel.send("🏓 Pong!");
 		message.channel.send(new Date().getTime() - message.createdTimestamp + "ms");
-		console.log("Ping-pong!".white);
+		console.log(chalk.hex("#ffffff")("Ping-pong!"));
 	}
 	if (command === "die") {
 		if(message.author.id !== settings.ownerID) {
 			message.channel.send(noperms);
-			console.log("Warning: A user without permissions tried to kill the bot.".yellow.bold);
+			console.log(chalk.yellow("Warning: A user without permissions tried to kill the bot."));
 			return;
 		}
 		message.channel.send("Goodbye! 👋");
@@ -110,19 +110,19 @@ client.on("message", message => {
 	}
 	if (command === "h") {
 		message.channel.send(cmds);
-		console.log("Help ran.".white);
+		console.log(chalk.hex("#ffffff")("Help ran."));
 	}
 	if (command === "help") {
 		message.channel.send(cmds);
-		console.log("Help ran.".white);
+		console.log(chalk.hex("#ffffff")("Help ran."));
 	}
 	if (command === settings.botShortName.toLowerCase()) {
 		message.channel.send(cmds);
-		console.log("Help ran.".white);
+		console.log(chalk.hex("#ffffff")("Help ran."));
 	}
 	if (command === "cmds") {
 		message.channel.send(cmds);
-		console.log("Help ran.".white);
+		console.log(chalk.hex("#ffffff")("Help ran."));
 	}
 	if (command === "invite") {
 		const invite = new Discord.RichEmbed()
@@ -132,7 +132,7 @@ client.on("message", message => {
 			.setDescription("Requested by " + message.author)
 			.setTimestamp();
 		message.channel.send(invite);
-		console.log(settings.botName + " had it's invite requested!".cyan);
+		console.log(settings.botName + chalk.cyan(" had it's invite requested!"));
 	}
 	if (command === "credits") {
 		const credits = new Discord.RichEmbed()
@@ -150,7 +150,7 @@ client.on("message", message => {
 			return;
 		}
 		client.user.setUsername(settings.botName);
-		console.log("Your bot name has been set to " + settings.botName.cyan + ".");
+		console.log("Your bot name has been set to " + settings.botName + ".");
 		message.react("✅");
 	}
 	if (command === "setavatar") {
@@ -159,7 +159,7 @@ client.on("message", message => {
 			return;
 		}
 		client.user.setAvatar(settings.avatar);
-		console.log("Your avatar has been set to ".zebra + settings.avatar + ".".zebra);
+		console.log("Your avatar has been set to " + settings.avatar + ".");
 		message.channel.send("✅");
 	}
 	// The Info command is also required to stay.
